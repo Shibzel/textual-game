7646603318ca7f64e00a3754af9b300a893c91a7
Shibzel <contact@shibzel.me>
2026-01-12 15:47:38 +0100

    Progress on the saving system

--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 #include "file_loader.h"
 #include "file_printer.h"
 
+#define SAVE_FP "./saves"
+#define ASSETS_FP "./assets"
+#define ASSET_FILE "%c_%c.txt"
+
 
 unsigned get_choice(char *display, char *bad_input, unsigned max_inputs) {
     unsigned input, unvalid;
@@ -23,26 +27,38 @@ unsigned get_choice(char *display, char *bad_input, unsigned max_inputs) {
     return input;
 }
 
+#include <sys/stat.h>
+int mkdir_if_not_exists(char *path) {
+    // TODO: Déplacer cette fonction dans un module
+    int status = 0;
+    int exists = 0;
+
+    struct stat buffer;
+    exists = !stat(path, &buffer);
+    
+    if (!exists) {
+        status = mkdir(path, 0755);
+    }
+    return status;
+}
+
+void init_save(char *save_path, char *name) {
+    mkdir_if_not_exists(save_path);
+
+    // TODO: Implement
+}
+
 int main(void) {
     unsigned choice;
+
+    // Language selection
     char language[3];
 
-    printf("Hello Let's test \n");
-    char *eng_text = load_asset("en_000.txt");
-    if (eng_text != NULL) {
-        printf("yes loaded, mtn test de print \n");
-        print_lines(eng_text);
-        free(eng_text);
-    }
-    puts("Hello world!");
-    
-
     choice = get_choice(
-        "Please chose a language / Merci de choisir un langage :\n 1) English\n 2) French\nInput a value / Entrez une valeur [1-2] : ",
+        "Please chose a language / Merci de choisir un langage :\n 1) English\n 2) Français\nInput a value / Entrez une valeur [1-2] : ",
         "Bad input. Try again",
         2
     );
-   
     switch (choice) {
         case 1:
             strcpy(language, "en");
@@ -51,6 +67,15 @@ int main(void) {
             strcpy(language, "fr");
             break;
     }
+
+    // Save selection
+    // TODO: Implement
+
+    // Resume/start the game
+    // TODO: Implement
+
+
+    // mkdir_if_not_exists(SAVE_FP);
 
     // char *eng_text = load_asset("english.txt");
     // if (eng_text != NULL) {
