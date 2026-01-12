4039e06ed91292859a26b4290a2ac11255257b6d
Shibzel <contact@shibzel.me>
2026-01-12 14:23:58 +0100

    Adding get_choice func

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+
 #include "file_loader.h"
 
+
+unsigned get_choice(char *display, char *bad_input, unsigned max_inputs) {
+    unsigned input, unvalid;
+
+    do {
+        unvalid = 0;
+
+        printf(display, max_inputs);
+        if (scanf("%d", &input) != 1 || input > max_inputs) {
+            unvalid = 1;
+            puts(bad_input);
+        }
+        printf("\n");
+    } while (unvalid);
+    
+    return input;
+}
+
 int main() {
-    printf("Hello world \n");
-    char *eng_text = load_asset("english.txt");
+    puts("Hello world!");
+
+    get_choice("What do you do ? [1-%d] : ", "Bad input. Try again.", 5);
 }
