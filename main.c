a5d5d0a6b0cc8877599cf0f8f4d5b023bba12d98
Shibzel <contact@shibzel.me>
2026-01-12 14:51:29 +0100

    Language input in main

--- main.c
+++ main.c
@@ -22,14 +22,30 @@ unsigned get_choice(char *display, char *bad_input, unsigned max_inputs) {
 }
 
 int main() {
-    printf("Hello world \n");
-    char *eng_text = load_asset("english.txt");
-    if (eng_text != NULL) {
-        printf("yes loaded, mtn test de print \n");
-        print_lines(eng_text);
-        free(eng_text);
-    }
-    puts("Hello world!");
+    unsigned choice;
+    char language[2];
 
-    get_choice("What do you do ? [1-%d] : ", "Bad input. Try again.", 5);
+    choice = get_choice(
+        "Please chose a language / Merci de choisir un langage :\n 1) English\n 2) French\nEntrez une valeur / Input a value [1-2] : ",
+        "Bad input. Try again",
+        2
+    );
+    switch (choice) {
+        case 1:
+            language = "en";
+            break
+        case 2:
+            language = "fr";
+            break
+    }
+
+    // char *eng_text = load_asset("english.txt");
+    // if (eng_text != NULL) {
+    //     printf("yes loaded, mtn test de print \n");
+    //     print_lines(eng_text);
+    //     free(eng_text);
+    // }
+    // puts("Hello world!");
+
+    // get_choice("What do you do ? [1-%d] : ", "Bad input. Try again.", 5);
 }
