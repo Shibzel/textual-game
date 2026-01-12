61cac77279fec2a904bf7f75057be2386c2d862f
Shibzel <contact@shibzel.me>
2026-01-12 15:02:09 +0100

    Correcting some bullshit in main

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>  // Pour strcpy
+
 #include "file_loader.h"
 #include "file_printer.h"
 
@@ -11,7 +13,7 @@ unsigned get_choice(char *display, char *bad_input, unsigned max_inputs) {
         unvalid = 0;
 
         printf(display, max_inputs);
-        if (scanf("%d", &input) != 1 || input > max_inputs) {
+        if (scanf("%u", &input) != 1 || input > max_inputs) {
             unvalid = 1;
             puts(bad_input);
         }
@@ -21,22 +23,22 @@ unsigned get_choice(char *display, char *bad_input, unsigned max_inputs) {
     return input;
 }
 
-int main() {
+int main(void) {
     unsigned choice;
-    char language[2];
+    char language[3];
 
     choice = get_choice(
-        "Please chose a language / Merci de choisir un langage :\n 1) English\n 2) French\nEntrez une valeur / Input a value [1-2] : ",
+        "Please chose a language / Merci de choisir un langage :\n 1) English\n 2) French\nInput a value / Entrez une valeur [1-2] : ",
         "Bad input. Try again",
         2
     );
     switch (choice) {
         case 1:
-            language = "en";
-            break
+            strcpy(language, "en");
+            break;
         case 2:
-            language = "fr";
-            break
+            strcpy(language, "fr");
+            break;
     }
 
     // char *eng_text = load_asset("english.txt");
