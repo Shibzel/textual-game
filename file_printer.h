15639a041af02d8b45f1ac276878b5b32a853d8b
Hugono23 <hugono23@hotmail.com>
2026-01-12 14:25:12 +0100

    File Loader
    File loading test

---
+++ file_printer.h
@@ -0,0 +1,6 @@
+#ifndef FILE_PRINTER_H
+#define FILE_PRINTER_H
+
+void print_lines(char *text);
+
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "file_loader.h"
+#include "file_printer.h"
 
 int main() {
     printf("Hello world \n");
     char *eng_text = load_asset("english.txt");
+    if (eng_text != NULL) {
+        printf("yes loaded, mtn test de print \n");
+        print_lines(eng_text);
+        free(eng_text);
+    }
 }
